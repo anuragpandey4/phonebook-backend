61e00c6198bc00716839e67f5ff520e723053a8e
Anurag Pandey <anurag.sky2565@gmail.com>
2024-11-23 23:54:36 +0530

    uibuild

--- index.js
+++ index.js
@@ -91,5 +91,5 @@ app.post("/api/persons", (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`server is running at ${PORT}`);
 });
